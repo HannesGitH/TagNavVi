9ec6849977463f74a5b3dfd8fbb5861b8ddffdb1
hannes <hannes@gfzdfuz.de>
2021-02-06 17:19:59 +0100

    wrapper for env

--- Sources/TagNavVi/Navigation/TagNavigationView.swift
+++ Sources/TagNavVi/Navigation/TagNavigationView.swift
@@ -7,19 +7,65 @@
 
 import SwiftUI
 
-//TODO: add navigationBar
 public struct TagNavigationView<Tag:Hashable> : View {
 
     @EnvironmentObject var navs: NavigationStackCollection<Tag>
     let tag : Tag
+    var headline : AnyView?
+    var withHomeButton = false
     
     public init<Content:View>(tag : Tag , @ViewBuilder _ content: () -> Content){
         self.tag = tag
-        do {
-            try _ = navs.observeChildrenChanges()
-        } catch  {
-            _navs = EnvironmentObject<NavigationStackCollection<Tag>>()
-        }
+        self.body = AnyView(
+            TagNavigationView_(tag: self.tag, headline: self.headline, withHomeButton: self.withHomeButton){
+                AnyView(content())
+            }.environmentObject(NavigationStackCollection<Int>([:]))
+        )
+    }
+    public var body: AnyView
+    
+}
+
+extension TagNavigationView{
+    
+    public init(
+        tag : Tag ,
+        headline : AnyView?,
+        withHomeButton: Bool,
+        content: () -> AnyView
+    ){
+        self.tag = tag
+        self.headline = headline
+        self.withHomeButton = withHomeButton
+        self.body = AnyView(
+            TagNavigationView_(tag: self.tag, headline: self.headline, withHomeButton: self.withHomeButton){
+                content()
+            }.environmentObject(NavigationStackCollection<Int>([:]))
+        )
+    }
+    
+    public func withTitle<HeadlineV:View>(
+        withHomeButton : Bool = false,
+        @ViewBuilder _ headline: @escaping () -> HeadlineV
+    )->some View{
+        return TagNavigationView(
+            tag: self.tag,
+            headline : AnyView(headline()),
+            withHomeButton: withHomeButton,
+            content: {self.body}
+        )
+    }
+}
+
+
+//TODO: add navigationBar
+struct TagNavigationView_<Tag:Hashable> : View {
+
+    @EnvironmentObject var navs: NavigationStackCollection<Tag>
+    let tag : Tag
+    
+    init<Content:View>(tag : Tag , @ViewBuilder _ content: () -> Content){
+        self.tag = tag
         let newStack = (
             k:tag,
             v:NavigationStack(
@@ -36,7 +82,7 @@ public struct TagNavigationView<Tag:Hashable> : View {
     }
     var headline : AnyView?
     var withHomeButton = false
-    public var body: some View{
+    var body: some View{
         VStack{
             if let hl = headline {
                 TagNavigationBar(tag: tag, withHomeButton : withHomeButton){
@@ -49,14 +95,16 @@ public struct TagNavigationView<Tag:Hashable> : View {
     
 }
 
-extension TagNavigationView{
+extension TagNavigationView_{
     
-    public init(
+    init(
         tag : Tag ,
-        headline : AnyView,
+        headline : AnyView?,
         withHomeButton: Bool,
         content: () -> AnyView
     ){
+        self.headline = headline
+        self.withHomeButton = withHomeButton
         self.tag = tag
         /*navs = */navs.observeChildrenChanges().append(
             k: tag,
@@ -69,7 +117,7 @@ extension TagNavigationView{
         )
     }
     
-    public func withTitle<HeadlineV:View>(
+    func withTitle<HeadlineV:View>(
         withHomeButton : Bool = false,
         @ViewBuilder _ headline: @escaping () -> HeadlineV
     )->some View{
